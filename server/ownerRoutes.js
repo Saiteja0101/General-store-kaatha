56afb94449cf2d5de891e429826eec7e9ab2c6a2
Chippa Saiteja <csaiteja64@gmail.com>
2025-07-31 23:56:47 +0530

    file name changed

--- server/ownerRoutes.js
+++ server/ownerRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const fetchData = require('./routes/fetchData.js')
+const fetchDues = require('./routes/fetchDues.js')
 const addcustomer = require('./routes/addCustomer.js')
 const updateCustomer = require('./routes/updateCustomer.js')
 const removeCustomer = require('./routes/removeCustomer.js')
@@ -7,7 +7,7 @@ const dashboard = require('./routes/dashboard.js')
 
 const router = express.Router()
 
-router.use('/', fetchData)
+router.use('/', fetchDues)
 router.use('/', addcustomer)
 router.use('/', updateCustomer)
 router.use('/', removeCustomer)
