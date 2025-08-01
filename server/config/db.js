cceea79bc3e098ff904245ea344e9aef803c125b
Chippa Saiteja <csaiteja64@gmail.com>
2025-08-01 13:50:52 +0530

    updated db

--- server/config/db.js
+++ server/config/db.js
@@ -1,23 +1,33 @@
-const { Pool } = require('pg')
-require('dotenv').config()
+const { Pool } = require('pg');
+require('dotenv').config();
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-})
+const isProduction = process.env.NODE_ENV === 'production';
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      }
+);
 
 const connectToDB = async () => {
-    try {
-        await pool.connect()
-        console.log("DB connected");
-        
-    } catch (err) {
-        console.log("error: ", err)
-    }
-}
-connectToDB()
+  try {
+    await pool.connect();
+    console.log('DB connected');
+  } catch (err) {
+    console.error('DB connection error:', err);
+  }
+};
+connectToDB();
 
-module.exports = pool
+module.exports = pool;
